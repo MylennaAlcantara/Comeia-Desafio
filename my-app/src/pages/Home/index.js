a328a7786e0f7f1009d1aac799d8d56cc2d02214
MylennaAlcantara <mylenna.a.a.corinthiana@gmail.com>
2022-03-09 10:06:42 -0300

    main

--- my-app/src/pages/Home/index.js
+++ my-app/src/pages/Home/index.js
@@ -1,12 +1,35 @@
 import '../Home/home.css';
 import {Link} from 'react-router-dom';
 import Slide from '../Slider';
+import ImageGallery from 'react-image-gallery';
+import React, { Component } from 'react';
 
 export const Home = () => {
+    const images = [
+        {
+            original: 'C:\Users\Annelym\Desktop\Log-in-Sing-up\my-app\src\Slider\images\imagem.jpg',
+            thumbnail: '',
+          },
+          {
+            original: 'C:\Users\Annelym\Desktop\Log-in-Sing-up\my-app\src\Slider\images\imagem2.jpg',
+            thumbnail: '',
+          },
+          {
+            original: 'C:\Users\Annelym\Desktop\Log-in-Sing-up\my-app\src\Slider\images\imagem3.jpg',
+            thumbnail: '',
+          },
+      ];
+      
+      class MyGallery extends React.Component {
+        render() {
+          return <ImageGallery items={images} />;
+        }
+      }
+    
     return(
         <div className="wrapper">
             <div className="slider">
-            <Slide/>
+                <MyGallery/>
             </div>
             <div className="login">
                 <h1>Entrar</h1>
